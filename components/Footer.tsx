65a2d425012de07a74420f6d437ffd159e001dbc
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-09-11 05:13:21 -0300

    add more elements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,22 +4,19 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-800 text-white py-12">
+    <footer className="bg-slate-800 text-white py-6 sm:py-12">
       <div className="container mx-auto px-4">
-        <div className="flex flex-wrap justify-between items-center mb-6">
-          <div className="flex items-center">
-            <h1 className="text-2xl font-bold mr-4">Kevin Duhamel</h1>
-            <p className="text-sm">&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
+        <div className="flex flex-col sm:flex-row justify-between items-center mb-6">
+          <div className="flex flex-col sm:flex-row items-center mb-6 sm:mb-0">
+            <h1 className="text-2xl font-bold mb-2 sm:mb-0 sm:mr-4">Kevin Duhamel</h1>
+            <p className="text-sm text-center sm:text-left">&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
           </div>
-          <div className="flex items-center space-x-4">
+          <div className="flex justify-center space-x-4">
             <Button as="a" href="https://github.com/kevinduhamel" color="light" auto aria-label="Github profile" target="_blank" rel="noopener noreferrer">
-              <FaGithub />
+              <FaGithub size={60} />
             </Button>
             <Button as="a" href="https://linkedin.com/in/kevinduhamel" color="light" auto aria-label="LinkedIn profile" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin />
-            </Button>
-            <Button as="a" href="https://twitter.com/kevinduhamel" color="light" auto aria-label="Twitter profile" target="_blank" rel="noopener noreferrer">
-              <FaTwitter />
+              <FaLinkedin size={60} />
             </Button>
           </div>
         </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,20 +4,20 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <main className='w-full h-full bg-slate-300 flex flex-col md:flex-row items-center justify-center p-6 space-y-6 md:space-y-0 md:space-x-8 transition-all duration-500 ease-in-out'>    
+    <main className='w-full h-full gap-10 p-12 bg-slate-300 flex flex-col md:flex-row items-center justify-center  space-y-6 md:space-y-0 md:space-x-8 transition-all duration-500 ease-in-out'>    
       <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" className="w-60 h-60 text-large transform hover:scale-105" />
       <div className='text-black space-y-4'>
         <h1 className='text-4xl font-bold tracking-wide'>Kevin Duhamel Hayes</h1>
         <h2 className='text-2xl font-semibold'>Full Stack Developer</h2>
         <h3 className='text-lg italic'>In constant learning</h3>
-        <p className='text-sm md:max-w-md'>
-          Passionate about web technologies and always eager to explore new tools. Experienced in building scalable and efficient web applications using modern frameworks.
+        <p className='text-m md:max-w-md'>
+        ¡Hola! Soy Kevin, apasionado por las tecnologías web y siempre ansioso por explorar nuevas herramientas. Con experiencia en la construcción de aplicaciones web escalables y eficientes utilizando marcos modernos. Este es mi escaparate digital, donde presento algunos de mis proyectos de desarrollo más interesantes. Estoy emocionado de compartirlos contigo y espero que disfrutes tu visita.
         </p>
         <div className="flex space-x-4 mt-4">
-          <Button color="secondary" variant='bordered' auto onClick={() => window.open('https://github.com/yourUsername', '_blank')} className="transform hover:scale-105 transition-transform duration-300">
+          <Button color="secondary" variant='bordered' auto onClick={() => window.open('https://github.com/kevinduhamelhayes', '_blank')} className="px-10 py-5 border-2 text-xl transform hover:scale-105 transition-transform duration-300">
             <FaGithub className="mr-2" /> Github
           </Button>
-          <Button color="primary" variant= "bordered" auto onClick={() => window.open('https://linkedin.com/in/yourProfile', '_blank')} className="transform hover:scale-105 transition-transform duration-300">
+          <Button color="primary" variant= "bordered" auto onClick={() => window.open('www.linkedin.com/in/kevin-duhamel-hayes', '_blank')} className="px-10 py-5 border-2 text-xl transform hover:scale-105 transition-transform duration-300">
             <FaLinkedin className="mr-2" /> LinkedIn
           </Button>
         </div>
